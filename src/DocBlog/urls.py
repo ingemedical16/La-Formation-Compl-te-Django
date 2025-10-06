28e4127a97a69a43bf2b614013fe44a86641ad0b
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-10-06 11:30:02 +0200

    feat: define application's URL patterns

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import index
 
 
+
+
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('blog/',include('blog.urls')),  # include the blog app URLs),
     path('admin/', admin.site.urls), 
 ]
