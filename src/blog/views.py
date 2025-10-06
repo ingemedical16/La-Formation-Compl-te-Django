1b485d3160579b9cd703b7ea900745aed4cd5dde
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-10-06 12:09:04 +0200

    feat(blog): add view for article pages

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,3 +7,8 @@ def index(request):
        'date':datetime.now(),
    }
    )
+   
+def article(request, numero_article):
+    if numero_article in ["01", "02", "03"]:
+        return render(request, f"blog/article_{numero_article}.html", context={})
+    return render(request, "blog/article_not_found.html")
